892ab8cbd3f886be908f277349235a07d942eacf
Kevsely <elegis.sossou@yahoo.com>
2022-01-13 07:55:30 +0100

    Removing extra comments

--- src/containers/App.js
+++ src/containers/App.js
@@ -5,22 +5,8 @@ import Scroll from '../components/Scroll';
 import './App.css';
 
 function App() {
-  // constructor() {
-  //   super()
-  //   this.state = {
-  //     robots: [],
-  //     searchfield: ''
-  //   }
-  // }
-
   const [robots, setRobots] = useState([])
   const [searchfield, setSearchfield] = useState('')
-
-  // componentDidMount() {
-  //   fetch('https://jsonplaceholder.typicode.com/users')
-  //     .then(response => response.json())
-  //     .then(users => { robots: users });
-  // }
 
   useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/users')
@@ -31,8 +17,7 @@ function App() {
   const onSearchChange = (event) => {
     setSearchfield(event.target.value);
   }
-
-  // const { robots, searchfield } = this.state;
+  
   const filteredRobots = robots.filter(robot => {
     return robot.name.toLowerCase().includes(searchfield.toLowerCase());
   })
